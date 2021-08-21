5dde2401768362127cb834195d736d657c5be073
Jose Salazar <solisjose466@gmail.com>
2021-08-20 17:46:31 -0700

    first

---
+++ lab1/Lab1.java
@@ -0,0 +1,18 @@
+//***************************************************************
+// Name: Jose Solis Salazar
+// Lab Letter:
+// Title: Lab1.java
+// Description: Prints out My first program and my name.
+// Date: 08/25/2021
+//***************************************************************
+
+public class Lab1
+{
+	public static void main (String [] args)
+	{
+//		System.out.print("Hello, my name is James Bond.");
+//		System.out.print("This is my first program!");
+		System.out.println("Hello, my name is James Bond.");
+		System.out.println("This is my first program!");
+	}
+}
