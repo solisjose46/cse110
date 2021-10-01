ae94710f2798f64f59196bb72cc376090056b300
Jose Salazar <solisjose466@gmail.com>
2021-10-01 16:48:51 -0700

    hw 3 complete

--- hw/assignment_3/Assignment3.java
+++ hw/assignment_3/Assignment3.java
@@ -8,7 +8,7 @@
 
 import java.util.Scanner;
 
-public class Assignment2
+public class Assignment3
 {
 	public static void main (String [] args)
 	{
@@ -49,30 +49,84 @@ public class Assignment2
 		//****************
 		
 		char command;
-		Scanner in = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		
+		//thse will help count grades entered
 		int aCount = 0;
 		int bCount = 0;
 		int cCount = 0;
+		
+		//since the values of the grade type doesnt need to change then set as constants
+		final int A_VALUE = 4;
+		final int B_VALUE = 3;
+		final int C_VALUE = 2;
+
+		//initialize gpa to zero
+		double gpa = 0;
 
 		do{
+			//get users command
 			System.out.println("Choose (A: add grades), (N: new grades), or (Q: quit)");
-			command = in.next.toUpperCase().charAt(0);
-			if(command == 'A'){
-				System.out.println("Type the additional input in single line");
-				while(input.hasNext()){
-					if(input.next().toUpperCase.charAt(0) == 'A'){
-						aCount++;
-					}
-					else if(input.next().toUpperCase.charAt(0) == 'B'){
-						bCount++;
-					}
-					else if(input.next().toUpperCase.charAt(0) == 'C'){
-						cCount++;
+			command = input.next().toUpperCase().charAt(0);
+			if(command == 'A' || command == 'N'){
+				if(command == 'A'){
+					System.out.println("Type the additional input in single line");			
+				}
+				else{
+					System.out.println("Type a new list of input in single line");
+					//reset the grade count
+					aCount = 0;
+					bCount = 0;
+					cCount = 0;
+				}
+
+				//grab the line user inputs to calculate gpa
+				if(input.hasNextLine()){
+					//skip \n
+					input.nextLine();
+					//this gets the next line of characters
+					String grades = input.nextLine();
+					grades = grades.toUpperCase();
+					
+					//process each grade
+					for(int i = 0; i < grades.length(); i++){
+						char grade = grades.charAt(i);
+						if(grade == 'A'){
+							aCount++;
+						}
+						else if(grade == 'B'){
+							bCount++;
+						}
+						else if(grade == 'C'){
+							cCount++;
+						}
 					}
 				}
-			}
-			else if(command == 'N'){
-				System.out.println("Type a new list of input in single line");
+
+				//calculate gpa
+				gpa = (double) ((A_VALUE * aCount) + (B_VALUE * bCount) + (C_VALUE * cCount)) / (aCount + bCount + cCount);
+				
+				//then print the grades *
+				System.out.println("----------------------");
+				System.out.print("\tA|");
+				for(int i = 0; i < aCount; i++){
+					System.out.print("*");
+				}
+				System.out.println();
+				System.out.print("\tB|");
+				for(int i = 0; i < bCount; i++){
+					System.out.print("*");
+				}
+				System.out.println();
+				System.out.print("\tC|");
+				for(int i = 0; i < cCount; i++){
+					System.out.print("*");
+				}
+				System.out.println();
+				System.out.println("----------------------");
+				//formats double to two decimal places
+				System.out.println("GPA: = " + String.format("%.2f",gpa));
+
 			}
 			else if(command == 'Q'){
 				System.out.println("*** End of program ***");
@@ -80,6 +134,9 @@ public class Assignment2
 			else{
 				System.out.println("Invalid command was input!");
 			}
+			System.out.println();
 		}while(command != 'Q');
+		//dont forget to close the scanner
+		input.close();
 	}
 }
